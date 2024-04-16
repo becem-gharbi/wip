5dae88056b8b57aeb26dc53b28b38a4fa7bc23a6
becem-gharbi <becem.gharbi@live.com>
2024-04-16 22:16:58 +0100

    refactor: change logo

--- layers/base/components/BasicLogo.vue
+++ layers/base/components/BasicLogo.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex items-center gap-3">
-    <naive-icon name="ph:person-simple-run" :size="25" />
+    <naive-icon name="noto:man-running-facing-right" :size="32" />
     <n-text strong>
       Work In Progress
     </n-text>
