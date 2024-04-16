0141f79c0398e22d0474dfe9b3cb583dcbd9a730
becem-gharbi <becem.gharbi@live.com>
2024-04-16 22:08:49 +0100

    refactor: display progress on project card

--- components/ProjectCard.vue
+++ components/ProjectCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <n-card class="cursor-pointer w-52" hoverable size="small" @click="$router.push('/projects/1')">
+  <n-card class="cursor-pointer w-64" hoverable size="small" @click="$router.push('/projects/1')">
     <template #header>
       <n-thing title="Project name" description="3 days ago">
         <template #avatar>
@@ -9,5 +9,11 @@
         </template>
       </n-thing>
     </template>
+
+    <n-progress
+      type="line"
+      :percentage="60"
+      indicator-placement="outside"
+    />
   </n-card>
 </template>
