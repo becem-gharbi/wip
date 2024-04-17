8a9b5ae862834668c69d7e5dddac07596995bab3
becem-gharbi <becem.gharbi@live.com>
2024-04-17 11:24:22 +0100

    feat: add chat modal

--- pages/projects/[id].vue
+++ pages/projects/[id].vue
@@ -5,20 +5,31 @@
     </template>
 
     <template #extra>
-      <n-button text @click="showProjectModal = true">
-        <template #icon>
-          <naive-icon name="ph:pen" />
-        </template>
-        Edit
-      </n-button>
+      <div class="flex gap-4">
+        <n-button text @click="showProjectModal = true">
+          <template #icon>
+            <naive-icon name="ph:pen" />
+          </template>
+          Edit
+        </n-button>
+
+        <n-button text @click="showChatModal = true">
+          <template #icon>
+            <naive-icon name="ph:chat-circle-dots" />
+          </template>
+          Chat
+        </n-button>
+      </div>
     </template>
 
     <kanban-board />
     <issue-modal />
     <project-modal v-model:show="showProjectModal" />
+    <chat-modal v-model:show="showChatModal" />
   </n-page-header>
 </template>
 
 <script setup lang="ts">
 const showProjectModal = ref(false)
+const showChatModal = ref(false)
 </script>
