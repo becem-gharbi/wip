814ddf76431787e925358cab81ba8705ac3a36ea
becem-gharbi <becem.gharbi@live.com>
2024-04-17 11:26:03 +0100

    revert(kanban): remove issue icon

--- components/KanbanIssue.vue
+++ components/KanbanIssue.vue
@@ -2,12 +2,6 @@
   <div @dblclick="navigateTo({ query: { selectedIssue: '1' } })">
     <n-card hoverable class="cursor-pointer" size="small">
       {{ summary }}
-
-      <template #header>
-        <n-avatar size="small">
-          <naive-icon name="ph:target" />
-        </n-avatar>
-      </template>
     </n-card>
   </div>
 </template>
