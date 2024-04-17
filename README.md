cab40e0b09e50d6258f79595c955736ee54a2e43
becem-gharbi <becem.gharbi@live.com>
2024-04-17 12:59:54 +0100

    chore(schema): define data models

--- README.md
+++ README.md
@@ -19,3 +19,38 @@ The platform can be self-hosted for data privacy supporting serverless deploymen
 - An owner can reject viewers per-project and revoke board sharing.
 - User can chat with other users per-project in realtime on the project messaging channel with one-to-many messages.
 - The chat channel supports only text messaging.
+
+## Models
+
+- User
+  - name
+  - email
+  - photo
+  - projects (relation)
+  - teams (relation)
+  - messages (relation)
+- Project
+  - name
+  - description
+  - createdAt
+  - updatedAt
+  - owner (relation)
+  - issues (relation)
+  - team (relation)
+- Team
+  - project
+  - users
+  - channel
+  - messages
+- Issue
+  - status
+  - summary
+  - description
+  - createdAt
+  - updatedAt
+  - project (relation)
+- Message
+  - content
+  - createdAt
+  - author
+  - team (relation)
---
+++ migrations/1713354729422.sql
@@ -0,0 +1,97 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "picture" TEXT NOT NULL,
+    "role" TEXT NOT NULL DEFAULT 'user',
+    "provider" TEXT NOT NULL DEFAULT 'default',
+    "password" TEXT,
+    "verified" BOOLEAN NOT NULL DEFAULT false,
+    "suspended" BOOLEAN NOT NULL DEFAULT false,
+    "requestedPasswordReset" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "RefreshToken" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "uid" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "userAgent" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "RefreshToken_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Project" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "ownerId" TEXT NOT NULL,
+    CONSTRAINT "Project_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Team" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "channel" TEXT NOT NULL,
+    "projectId" TEXT NOT NULL,
+    CONSTRAINT "Team_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Issue" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "status" TEXT NOT NULL DEFAULT 'backlog',
+    "summary" TEXT NOT NULL,
+    "description" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "projectId" TEXT NOT NULL,
+    CONSTRAINT "Issue_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Message" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "content" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "teamId" INTEGER NOT NULL,
+    "authorId" TEXT NOT NULL,
+    CONSTRAINT "Message_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Message_teamId_fkey" FOREIGN KEY ("teamId") REFERENCES "Team" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "_TeamToUser" (
+    "A" INTEGER NOT NULL,
+    "B" TEXT NOT NULL,
+    CONSTRAINT "_TeamToUser_A_fkey" FOREIGN KEY ("A") REFERENCES "Team" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "_TeamToUser_B_fkey" FOREIGN KEY ("B") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE INDEX "RefreshToken_userId_idx" ON "RefreshToken"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Project_ownerId_name_key" ON "Project"("ownerId", "name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Team_channel_key" ON "Team"("channel");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Team_projectId_key" ON "Team"("projectId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_TeamToUser_AB_unique" ON "_TeamToUser"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_TeamToUser_B_index" ON "_TeamToUser"("B");
