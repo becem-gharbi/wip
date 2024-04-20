48ae21f81750b9a707fabb98ef25a5847109a08d
becem-gharbi <becem.gharbi@live.com>
2024-04-20 18:45:48 +0100

    refactor: create separate component for messages display in realtime

--- layers/data/server/api/messages/index.get.ts
+++ layers/data/server/api/messages/index.get.ts
@@ -1,20 +1,14 @@
 export default defineEventHandler((event) => {
   const { userId } = checkAuth(event)
 
-  const query = getQuery<{ teamId: string }>(event)
+  const query = getQuery<{ teamId: string; userId: string }>(event)
 
   return event.context.prisma.message.findMany({
     where: {
-      team: {
-        id: query.teamId,
-        users: {
-          some: {
-            id: {
-              equals: userId
-            }
-          }
-        }
-      }
+      authorId: {
+        in: [userId, query.userId]
+      },
+      teamId: query.teamId
     },
     orderBy: {
       createdAt: 'asc'
