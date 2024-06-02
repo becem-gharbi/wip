706f1b092b418d8117b0edb279f6ae58d766e90f
becem-gharbi <becem.gharbi@live.com>
2024-06-02 11:00:35 +0100

    chore: add source code btn

--- pages/home.vue
+++ pages/home.vue
@@ -13,7 +13,29 @@
           to="/auth/login"
           class="inline-flex items-center px-6 py-4 mt-8 font-semibold text-black transition-all duration-200 bg-yellow-300 rounded-full lg:mt-16 hover:bg-yellow-400 focus:bg-yellow-400"
         >
-          Get started
+          Get Started
+          <svg
+            class="w-6 h-6 ml-8 -mr-2"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="1.5"
+              d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+        </nuxt-link>
+
+        <nuxt-link
+          :to="repository"
+          target="_blank"
+          class="ml-2 inline-flex items-center px-6 py-4 mt-8 font-semibold text-white transition-all duration-200 bg-blue-500 rounded-full lg:mt-16 hover:bg-blue-400 focus:bg-blue-400"
+        >
+          Source Code
           <svg
             class="w-6 h-6 ml-8 -mr-2"
             xmlns="http://www.w3.org/2000/svg"
@@ -57,6 +79,8 @@
 </template>
 
 <script setup lang="ts">
+import { repository } from '../package.json'
+
 definePageMeta({
   middleware: 'guest',
   colorMode: 'light',
